becfa3c3bd6b0691a256aa9dc36ed3d5f69529ff
Ana Paula Junqueira <anapaula.junq@hotmail.com>
2024-09-10 22:10:49 -0300

    tela cadastro

--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,10 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../api';
+import './RegisterPage.css';
+import logoImage from '../../assets/LOGO_INFOWORD.png';
 
 const RegisterPage = () => {
+  const [formData, setFormData] = useState({
+    nome: '',
+    email: '',
+    senha: '',
+  });
+
+  const [errors, setErrors] = useState({
+    nome: '',
+    email: '',
+    senha: '',
+  });
+
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setErrors({
+      nome: '',
+      email: '',
+      senha: '',
+    });
+    setMessage('');
+
+    let hasErrors = false;
+    if (!formData.nome) {
+      setErrors((prev) => ({ ...prev, nome: 'Insira um nome' }));
+      hasErrors = true;
+    }
+    if (!formData.email) {
+      setErrors((prev) => ({ ...prev, email: 'Insira um email' }));
+      hasErrors = true;
+    }
+    if (!formData.senha) {
+      setErrors((prev) => ({ ...prev, senha: 'Insira uma senha' }));
+      hasErrors = true;
+    }
+
+    if (!hasErrors) {
+      try {
+        await api.post('/usuarios', {
+          usuario: {
+            nome: formData.nome,
+            email: formData.email,
+            senha: formData.senha,
+          },
+        });
+        setMessage('Cadastrado com sucesso!');
+      } catch (error) {
+        setMessage('Erro ao cadastrar. Tente novamente.');
+      }
+    }
+  };
+
   return (
-    <div>
-      <h1>Cadastro</h1>
-      {/* Formulário de Cadastro vai aqui */}
+    <div className="container">
+      <img src={logoImage} alt="Logo InfoWord" />
+
+      <h1>Cadastre-se</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="nome">Nome:</label>
+          <input
+            type="text"
+            id="nome"
+            name="nome"
+            value={formData.nome}
+            onChange={handleChange}
+            placeholder="Digite seu nome"
+          />
+          {errors.nome && <p>{errors.nome}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="Digite seu email"
+          />
+          {errors.email && <p>{errors.email}</p>}
+        </div>
+
+        <div>
+          <label htmlFor="senha">Senha:</label>
+          <input
+            type="password"
+            id="senha"
+            name="senha"
+            value={formData.senha}
+            onChange={handleChange}
+            placeholder="Digite sua senha"
+          />
+          {errors.senha && <p>{errors.senha}</p>}
+        </div>
+
+        <button type="submit">Cadastrar</button>
+      </form>
+
+      {message && (
+        <p
+          className={`message ${
+            message.includes('sucesso') ? 'success' : 'error'
+          }`}
+        >
+          {message}
+        </p>
+      )}
     </div>
   );
 };
